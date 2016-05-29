652d4906f3ac59b79692ef77540c003217494fae
Sameer Akhtar <sameervitian2006@gmail.com>
2016-05-30 00:19:06 +0700

    added preparex support

--- sqlt.go
+++ sqlt.go
@@ -18,6 +18,12 @@ type DB struct {
 	count      uint64
 }
 
+//Stmt implement sql stmt
+type Stmt struct {
+	db    *DB
+	stmts []*sql.Stmt
+}
+
 //Open connection to database
 func Open(driverName, sources string) (*DB, error) {
 	var err error
@@ -58,6 +64,47 @@ func (db *DB) Ping() error {
 	return nil
 }
 
+//Preare will return sql stmt
+func (db *DB) Prepare(query string) (Stmt, error) {
+	var err error
+	stmt := Stmt{}
+	stmts := make([]*sql.Stmt, len(db.sqlxdb))
+
+	for i := range db.sqlxdb {
+		stmts[i], err = db.sqlxdb[i].Prepare(query)
+
+		if err != nil {
+			return stmt, err
+		}
+	}
+
+	stmt.db = db
+	stmt.stmts = stmts
+	return stmt, nil
+}
+
+//Preparex sqlx stmt
+func (db *DB) Preparex(query string) (*Stmtx, error) {
+	var err error
+	stmts := make([]*sqlx.Stmt, len(db.sqlxdb))
+
+	for i := range db.sqlxdb {
+		stmts[i], err = db.sqlxdb[i].Preparex(query)
+
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return &Stmtx{db: db, stmts: stmts}, nil
+}
+
+func (db *DB) SetMaxOpenConnections(max int) {
+	for i := range db.sqlxdb {
+		db.sqlxdb[i].SetMaxOpenConns(max)
+	}
+}
+
 //Slave return slave database
 func (db *DB) Slave() *sqlx.DB {
 	return db.sqlxdb[db.slave(db.length)]
@@ -68,20 +115,16 @@ func (db *DB) Master() *sqlx.DB {
 	return db.sqlxdb[0]
 }
 
-func (db *DB) SetMaxOpenConnections(max int) {
-	for i := range db.sqlxdb {
-		db.sqlxdb[i].SetMaxOpenConns(max)
-	}
+// Queryx queries the database and returns an *sqlx.Rows.
+func (db *DB) Query(query string, args ...interface{}) (*sql.Rows, error) {
+	r, err := db.sqlxdb[db.slave(db.length)].Query(query, args...)
+	return r, err
 }
 
-// Select using this DB.
-func (db *DB) Select(dest interface{}, query string, args ...interface{}) error {
-	return db.sqlxdb[db.slave(db.length)].Select(dest, query, args...)
-}
-
-// Get using this DB.
-func (db *DB) Get(dest interface{}, query string, args ...interface{}) error {
-	return db.sqlxdb[db.slave(db.length)].Get(dest, query, args...)
+// QueryRowx queries the database and returns an *sqlx.Row.
+func (db *DB) QueryRow(query string, args ...interface{}) *sql.Row {
+	rows := db.sqlxdb[db.slave(db.length)].QueryRow(query, args...)
+	return rows
 }
 
 // Queryx queries the database and returns an *sqlx.Rows.
@@ -96,10 +139,24 @@ func (db *DB) QueryRowx(query string, args ...interface{}) *sqlx.Row {
 	return rows
 }
 
+func (db *DB) Exec(query string, args ...interface{}) (sql.Result, error) {
+	return db.sqlxdb[0].Exec(query, args...)
+}
+
 //Using master db
 // MustExec (panic) runs MustExec using this database.
 func (db *DB) MustExec(query string, args ...interface{}) sql.Result {
 	return db.sqlxdb[0].MustExec(query, args...)
+}
+
+// Select using this DB.
+func (db *DB) Select(dest interface{}, query string, args ...interface{}) error {
+	return db.sqlxdb[db.slave(db.length)].Select(dest, query, args...)
+}
+
+// Get using this DB.
+func (db *DB) Get(dest interface{}, query string, args ...interface{}) error {
+	return db.sqlxdb[db.slave(db.length)].Get(dest, query, args...)
 }
 
 //Using master db
@@ -119,44 +176,7 @@ func (db *DB) MustBegin() *sqlx.Tx {
 	return tx
 }
 
-//Preare will return sql stmt
-func (db *DB) Prepare(query string) (Stmt, error) {
-	var err error
-	stmt := Stmt{}
-	stmts := make([]*sql.Stmt, len(db.sqlxdb))
-
-	for i := range db.sqlxdb {
-		stmts[i], err = db.sqlxdb[i].Prepare(query)
-
-		if err != nil {
-			return stmt, err
-		}
-	}
-
-	stmt.db = db
-	stmt.stmts = stmts
-	return stmt, nil
-	//return &Stmt{db: db, stmts: stmts}, nil
-}
-
-//Stmt implement sql stmt
-type Stmt struct {
-	db    *DB
-	stmts []*sql.Stmt
-}
-
-//Close stmt
-func (st *Stmt) Close() error {
-	for i := range st.stmts {
-		err := st.stmts[i].Close()
-
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
+/*******************************************/
 
 //Exec will always go to production
 func (st *Stmt) Exec(args ...interface{}) (sql.Result, error) {
@@ -173,20 +193,71 @@ func (st *Stmt) QueryRow(args ...interface{}) *sql.Row {
 	return st.stmts[st.db.slave(len(st.db.sqlxdb))].QueryRow(args...)
 }
 
-//Preparex sqlx stmt
-func (db *DB) Preparex(query string) (*Stmtx, error) {
-	var err error
-	stmts := make([]*sqlx.Stmt, len(db.sqlxdb))
-
-	for i := range db.sqlxdb {
-		stmts[i], err = db.sqlxdb[i].Preparex(query)
+//Close stmt
+func (st *Stmt) Close() error {
+	for i := range st.stmts {
+		err := st.stmts[i].Close()
 
 		if err != nil {
-			return nil, err
+			return err
 		}
 	}
 
-	return &Stmtx{db: db, stmts: stmts}, nil
+	return nil
+}
+
+/********************************************/
+
+func (st *Stmtx) Close() error {
+	for i := range st.stmts {
+		err := st.stmts[i].Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+//Exec will always go to production
+func (st *Stmtx) Exec(args ...interface{}) (sql.Result, error) {
+	return st.stmts[0].Exec(args...)
+
+}
+
+//Query will always go to slave
+func (st *Stmtx) Query(args ...interface{}) (*sql.Rows, error) {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].Query(args...)
+}
+
+//QueryRow will always go to slave
+func (st *Stmtx) QueryRow(args ...interface{}) *sql.Row {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].QueryRow(args...)
+}
+
+func (st *Stmtx) MustExec(args ...interface{}) sql.Result {
+	return st.stmts[0].MustExec(args...)
+}
+
+//Query will always go to slave
+func (st *Stmtx) Queryx(args ...interface{}) (*sqlx.Rows, error) {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].Queryx(args...)
+}
+
+//QueryRow will always go to slave
+func (st *Stmtx) QueryRowx(args ...interface{}) *sqlx.Row {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].QueryRowx(args...)
+}
+
+//Query will always go to slave
+func (st *Stmtx) Get(dest interface{}, args ...interface{}) error {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].Get(dest, args...)
+}
+
+//QueryRow will always go to slave
+func (st *Stmtx) Select(dest interface{}, args ...interface{}) error {
+	return st.stmts[st.db.slave(len(st.db.sqlxdb))].Select(dest, args...)
 }
 
 //Stmtx implement sqlx stmt
