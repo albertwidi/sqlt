5f2fb1bd2c58343e997f3cebb75f1728f31e3349
albert-widi <albert.widiatmoko@gmail.com>
2017-04-12 14:42:03 +0700

    Fix ping

--- sqlt_context.go
+++ sqlt_context.go
@@ -76,7 +76,15 @@ func (db *DB) PingContext(ctx context.Context) error {
 	var err error
 
 	if !db.heartBeat {
-		return db.PingContext(ctx)
+		if !db.heartBeat {
+			for _, val := range db.sqlxdb {
+				err = val.PingContext(ctx)
+				if err != nil {
+					return err
+				}
+			}
+			return err
+		}
 	}
 
 	for i := 0; i < len(db.activedb); i++ {
